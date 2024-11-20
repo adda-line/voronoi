69130ac924b21d7d88349e21377862ee55d874b6
add_a_line <huntmccu@gmail.com>
2024-11-19 16:51:23 -0800

    Made the point draw itself - Added coordinate printing too :)

--- src/UI/Point.cs
+++ src/UI/Point.cs
@@ -2,4 +2,14 @@ using Godot;
 
 public partial class Point : Area2D
 {
+    private const float POINT_RADIUS = 3.0f;
+
+    /// <inheritdoc/>
+    public override void _Draw()
+    {
+        // TODO: Looking at canvasPos while debugging, why is it (0,0)?
+        var canvasPos = MakeCanvasPositionLocal(Position);
+        DrawCircle(canvasPos, POINT_RADIUS, Colors.Red);
+        DrawString(ThemeDB.FallbackFont, canvasPos, $"({(int)GlobalPosition.X},{(int)GlobalPosition.Y})");
+    }
 }
