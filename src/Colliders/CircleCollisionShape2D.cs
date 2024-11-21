559e1b1e1a234396ba246859048d2fb1035da549
add_a_line <adaline.mccu@gmail.com>
2024-11-21 09:03:35 -0800

    This turned out better than I expected

---
+++ src/Colliders/CircleCollisionShape2D.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+/// <summary>
+/// Just a <see cref="CollisionShape2D"/> node that has a <see cref="CircleShape2D"/> shape.
+/// </summary>
+public partial class CircleCollisionShape2D : CollisionShape2D
+{
+    private readonly CircleShape2D _circleShape;
+
+    private float _radius;
+
+    /// <summary>
+    /// Radius of the internal <see cref="CircleShape2D"/>.
+    /// </summary>
+    public float Radius
+    {
+        get => _radius;
+        set
+        {
+            _radius = value;
+            _circleShape.Radius = value;
+        }
+    }
+
+    public CircleCollisionShape2D(float radius)
+    {
+        _radius = radius;
+        Shape = _circleShape = new CircleShape2D()
+        {
+            Radius = _radius
+        };
+    }
+}
--- src/UI/Point.cs
+++ src/UI/Point.cs
@@ -2,18 +2,17 @@ using Godot;
 
 public partial class Point : Area2D
 {
-    private float _radius;
+    private readonly CircleCollisionShape2D _collider;
+
     private Color _color;
     private bool _showCoords;
 
     public float Radius
     {
-        get => _radius;
+        get => _collider.Radius;
         set
         {
-            // TODO: This feels hacky but I'm not sure what would be better....
-            _radius = Mathf.Abs(value);
-            ((CircleShape2D)GetChild<CollisionShape2D>(0, true).Shape).Radius = value;
+            _collider.Radius = Mathf.Abs(value);
             QueueRedraw();
         }
     }
@@ -40,27 +39,21 @@ public partial class Point : Area2D
 
     public Point(float radius, Color color, bool showCoords)
     {
-        _radius = Mathf.Abs(radius);
         _color = color;
         _showCoords = showCoords;
 
-        AddChild(new CollisionShape2D()
-        {
-            Shape = new CircleShape2D()
-            {
-                Radius = _radius
-            }
-        }, @internal: InternalMode.Front);
+        _collider = new CircleCollisionShape2D(Mathf.Abs(radius));
+        AddChild(_collider, @internal: InternalMode.Front);
     }
 
     /// <inheritdoc/>
     public override void _Draw()
     {
         // We're drawing in local coords, NOT the containing canvas' coords.
-        DrawCircle(Vector2.Zero, _radius, _color);
+        DrawCircle(Vector2.Zero, _collider.Radius, _color);
         if (_showCoords)
         {
-            this.DrawCenteredString(ThemeDB.FallbackFont, new Vector2(0, -(20 + _radius)), $"({(int)GlobalPosition.X}, {(int)GlobalPosition.Y})");
+            this.DrawCenteredString(ThemeDB.FallbackFont, new Vector2(0, -(20 + _collider.Radius)), $"({(int)GlobalPosition.X}, {(int)GlobalPosition.Y})");
         }
     }
 }
