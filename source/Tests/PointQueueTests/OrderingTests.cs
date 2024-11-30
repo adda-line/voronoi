faf3534fcc1fd193db19066a74eab55f4e11d2ad
add_a_line <adaline.mccu@gmail.com>
2024-11-30 01:03:51 -0800

    Pushing up first UTs, gonna have to have an interface for events being pushed

--- source/Tests/PointQueueTests/OrderingTests.cs
+++ source/Tests/PointQueueTests/OrderingTests.cs
@@ -1,3 +1,60 @@
 public class OrderingTests
 {
+    [Fact]
+    public void PointsSortedByY()
+    {
+        // Arrange
+        EventQueue q = new();
+        var first = new Godot.Vector2(0, 1);
+        var second = new Godot.Vector2(0, 2);
+        var third = new Godot.Vector2(0, 3);
+
+        // Act
+        q.Enqueue(second);
+        q.Enqueue(first);
+        q.Enqueue(third);
+
+        // Assert
+        Assert.Equal(3, q.Count);
+        Assert.Equal(first, q.Dequeue());
+        Assert.Equal(second, q.Dequeue());
+        Assert.Equal(third, q.Dequeue());
+    }
+
+    [Fact]
+    public void DeferToXWhenYsEqual()
+    {
+        // Arrange
+        EventQueue q = new();
+        var first = new Godot.Vector2(1, 0);
+        var second = new Godot.Vector2(2, 0);
+        var third = new Godot.Vector2(3, 0);
+
+        // Act
+        q.Enqueue(second);
+        q.Enqueue(first);
+        q.Enqueue(third);
+
+        // Assert
+        Assert.Equal(3, q.Count);
+        Assert.Equal(first, q.Dequeue());
+        Assert.Equal(second, q.Dequeue());
+        Assert.Equal(third, q.Dequeue());
+    }
+
+    [Fact]
+    public void ThrowOnDuplicatePoints()
+    {
+        // Arrange
+        EventQueue q = new();
+        var first = new Godot.Vector2(1, 1);
+        var second = new Godot.Vector2(1, 1);
+
+        // Assert on Act
+        q.Enqueue(first);
+        Assert.Throws<Exception>(() =>
+        {
+            q.Enqueue(second);
+        });
+    }
 }
---
+++ source/Voronoi/DataStructures/EventQueue.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("PointQueueTests")]
+internal class EventQueue
+{
+    public int Count => throw new NotImplementedException();
+
+    public void Enqueue(Vector2 _)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Vector2 Dequeue()
+    {
+        throw new NotImplementedException();
+    }
+}
